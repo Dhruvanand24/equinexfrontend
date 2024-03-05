dc84e10479ab5405a1e11d0afda89d4968fc81f3
Md Kaif Manzar <kaifmanzar321@gmail.com>
2024-03-05 15:18:02 +0530

    adding table

---
+++ src/components/Table.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Table = () => {
+  return (
+    <div>
+      <div id="tablehead" className='w-full flex text-[#595959] font-semibold text-sm py-2 border-b-[1px] border-[#D9D9D9]'>
+        <div className='w-10 flex text-wrap items-center justify-start'>S No.</div>
+        <div className='w-20 flex text-wrap items-center justify-start'>Order Id</div>
+        <div className='w-24 flex text-wrap items-center justify-start'>DOO</div>
+        <div className='w-28 flex text-wrap items-center justify-start'>Buyer</div>
+        <div className='w-20 flex text-wrap items-center justify-start'>Details</div>
+        <div className='w-28 flex text-wrap items-center justify-start'>Deal Amount</div>
+        <div className='w-28 flex text-wrap items-center justify-start'>Amount Paid</div>
+        <div className='w-24 flex text-wrap items-center justify-start'>Amount Due</div>
+        <div className='w-24 flex text-wrap items-center justify-start'>Dead line</div>
+        <div className='w-24 flex text-wrap items-center justify-start'>Delivery Date</div>
+        <div className='w-14 flex text-wrap items-center justify-start'>Recipt</div>
+      </div>
+
+
+
+      <div id="table body" className='w-full flex text-[#595959] text-sm py-2 border-b-[1px] border-[#D9D9D9]'>
+        <div className='w-10 overflow-x-auto overflow-y-hidden text-nowrap flex items-center justify-start'>1.</div>
+        <div className='w-20 overflow-x-auto overflow-y-hidden text-nowrap flex items-center justify-start'>xxx001</div>
+        <div className='w-24 overflow-x-auto overflow-y-hidden text-nowrap flex items-center justify-start'>21-02-2024</div>
+        <div className='w-28 overflow-x-auto overflow-y-hidden text-nowrap flex items-center justify-start'>xyz@ pvt. ltd</div>
+        <div className='w-20 overflow-x-auto overflow-y-hidden text-nowrap flex items-center justify-start'>vew </div>
+        <div className='w-28 overflow-x-auto overflow-y-hidden text-nowrap flex items-center justify-start'>2000000</div>
+        <div className='w-28 overflow-x-auto overflow-y-hidden text-nowrap flex items-center justify-start'>1000000</div>
+        <div className='w-24 overflow-x-auto overflow-y-hidden text-nowrap flex items-center justify-start'>1000000</div>
+        <div className='w-24 overflow-x-auto overflow-y-hidden text-nowrap flex items-center justify-start'>01-05-2024</div>
+        <div className='w-24 overflow-x-auto overflow-y-hidden text-nowrap flex items-center justify-start'>NA</div>
+        <div className='w-14 overflow-x-auto overflow-y-hidden text-nowrap flex items-center justify-start'>Print</div>
+      </div>
+    </div>
+  )
+}
+
+export default Table
--- src/pages/OrderList.jsx
+++ src/pages/OrderList.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Table from '../components/Table'
 const OrderList = () => {
   return (
     <div className='bg-white p-4 w-full flex flex-col justify-start items-start'>
@@ -10,7 +11,7 @@ const OrderList = () => {
             <div className=' flex flex-col gap-1'>
                 <p className='text-[#718096] text-sm'>PR No.</p>
                 <div className='relative w-fit overflow-hidden'>
-                    <input className=' w-[256px] bg-[#BACFFF] text-[#8792A4] bg-opacity-25 rounded-sm border-[#BACFFF] border-[1px] px-4 py-2 cursor-text outline-blue-500' type="text" placeholder='Purchase Order No.'/>
+                    <input className=' w-[256px] bg-[#edf1fa] text-[#8792A4] rounded-sm border-[#D9D9D9] border-[1px] px-4 py-2 cursor-text outline-blue-500' type="text" placeholder='Purchase Order No.'/>
                     <img className='absolute top-[5px] right-1 text-xl h-8 cursor-pointer p-2' src="search.png" alt="" />
                 </div>
             </div>
@@ -19,7 +20,7 @@ const OrderList = () => {
             <div className=' flex flex-col gap-1'>
                 <p className='text-[#718096] text-sm'>From Date</p>
                 <div className='relative w-fit overflow-hidden'>
-                    <input className=' w-[256px] bg-[#BACFFF] text-[#8792A4] bg-opacity-30 rounded-sm border-[#D9D9D9] border-[1px] px-4 py-2 cursor-text outline-blue-500' type="text" placeholder='Purchase Order No.'/>
+                    <input className=' w-[256px] bg-[#edf1fa] text-[#8792A4] rounded-sm border-[#D9D9D9] border-[1px] px-4 py-2 cursor-text outline-blue-500' type="text" placeholder='Purchase Order No.'/>
                     <img className='absolute top-0 right-0 text-xl h-10 cursor-pointer' src="/icons/Date.svg" alt="" />
                 </div>
             </div>
@@ -28,7 +29,7 @@ const OrderList = () => {
             <div className=' flex flex-col gap-1'>
                 <p className='text-[#718096] text-sm'>To Date</p>
                 <div className='relative w-fit overflow-hidden'>
-                    <input className=' w-[256px] bg-[#BACFFF] text-[#8792A4] bg-opacity-30 rounded-sm border-[#D9D9D9] border-[1px] px-4 py-2 cursor-text outline-blue-500' type="text" placeholder='Purchase Order No.'/>
+                    <input className=' w-[256px] bg-[#edf1fa] text-[#8792A4] rounded-sm border-[#D9D9D9] border-[1px] px-4 py-2 cursor-text outline-blue-500' type="text" placeholder='Purchase Order No.'/>
                     <img className='absolute top-0 right-0 text-xl h-10 cursor-pointer' src="/icons/Date.svg" alt="" />
                 </div>
             </div>
@@ -37,7 +38,7 @@ const OrderList = () => {
        <button className='px-4 py-2 bg-blue-500 rounded-[5px] text-white abs relative  left-[85%] hover:bg-blue-600'>+ Create Order</button>
        
        <hr className='bg-blue-500 h-1 mt-4'/>
-       
+       <Table/>
       </div>
     </div>
   )
