796829d2b29719d735eb703618a0e10ba1c28911
Dhruv_Anand <ananddhruv475@gmail.com>
2024-03-05 14:14:34 +0530

    added navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='w-full flex h-[65px] px-8 items-center justify-between bg-white'>
+      <h1 className='font-[600] text-[24px]'>Bird machines Pvt.Ltd (Bhagola)</h1>
+      <h1>UserName</h1>
+    </div>
+  )
+}
+export default Navbar
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../components/Sidebar';
+import Navbar from '../components/Navbar';
 
 
 
@@ -8,7 +9,10 @@ const Main = () => {
   return (
       <div className='bg-background-0 h-screen font-roboto flex'>
         <Sidebar />
+        <div className='flex flex-col w-full'>
+          <Navbar />
         <Outlet />
+        </div>
       </div>
     
   )
