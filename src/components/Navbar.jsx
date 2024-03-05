f3f1eadfa7a6eb0c75b33ec3f95459c2f582a11d
Dhruv_Anand <ananddhruv475@gmail.com>
2024-03-05 15:25:25 +0530

    a

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='w-full flex h-[65px] px-8 items-center justify-between bg-white'>
+    <div className='w-full flex h-[65px] px-8 items-center justify-between bg-white border-b'>
       <h1 className='font-[600] text-[24px]'>Bird machines Pvt.Ltd (Bhagola)</h1>
       <h1>UserName</h1>
     </div>
