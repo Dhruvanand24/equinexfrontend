99b878897d7dc5117157113d4c56ffaa4968dbf1
Md Kaif Manzar <kaifmanzar321@gmail.com>
2024-03-07 19:32:52 +0530

    create material modal done

--- src/components/modals/CreateMaterial.model.jsx
+++ src/components/modals/CreateMaterial.model.jsx
@@ -1,4 +1,4 @@
-import { DatePicker , Alert, Spin } from "antd";
+import { DatePicker, Alert, Spin } from "antd";
 import axios from "axios";
 import { Item } from "rc-menu";
 import React, { useState } from "react";
@@ -7,9 +7,7 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 // import MultiSelectDropdown from '../MultiSelectDropdown.jsx';
 
 const CreateMaterial = () => {
-  
-    const [loading, setLoading] = useState(false);
-    
+  const [loading, setLoading] = useState(false);
 
   const {
     register,
@@ -22,42 +20,61 @@ const CreateMaterial = () => {
 
   const onSubmit = async (data) => {
     setLoading(true);
+    console.log(data);
+    const creationdata = {
+      Name: data.materialName,
+      description: data,
+    };
     try {
-        const response = await axios.post("http://localhost:8000/api/v1/users/register", data,{
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          });
-          console.log(response.data);
-          setLoading(false);
-          
+      const response = await axios.post(
+        "http://localhost:8000/api/v1/material/createMaterial",
+        creationdata,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log(response.data);
+      setLoading(false);
+      document.getElementById("create_new_material_modal").close()
+
     } catch (error) {
-        console.log("failed", error.response.data);
-        
+      console.log("failed", error.response.data);
+      setLoading(false);
     }
   };
 
-  const AllBuyers = [{ name: "Plant Manager" }, { name: "Store Manager" }, { name: "Super Admin" }, { name: "Production" }, { name: "Quality Tester" }];
+  const AllBuyers = [
+    { name: "Plant Manager" },
+    { name: "Store Manager" },
+    { name: "Super Admin" },
+    { name: "Production" },
+    { name: "Quality Tester" },
+  ];
 
   return (
     <dialog id="create_new_material_modal" className="modal backdrop-blur-sm">
-        
       <div className="modal-box w-3/5 h-[80%] max-w-5xl flex flex-col items-center rounded-lg ">
         <div className="modal-action h-full">
-          <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col h-full" method="dialog">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col h-full"
+            method="dialog"
+          >
             <h3 className="font-bold text-[18px] text-heading-0 mb-4">
               Add New Material
             </h3>
             <hr />
             <div className="flex flex-wrap gap-x-10 gap-y-7 mt-4 w-full items-center justify-start text-text-0 text-[18px] relative">
-            <div className=" flex flex-col gap-1 items-start justify-start">
+              <div className=" flex flex-col gap-1 items-start justify-start">
                 <p className="text-[14px] ">Name of Material</p>
                 <div className="w-fit overflow-hidden">
                   <input
                     className="text-[14px] w-[256px] bg-[#edf1fa] text-[#8792A4] rounded-lg border-ring-0 border-[1px] px-4 py-[8px] cursor-text outline-blue-500"
                     type="text"
                     placeholder="Material Name"
-                    {...register("materilaName", {required: true})}
+                    {...register("materialName", { required: true })}
                   />
                 </div>
               </div>
@@ -69,10 +86,13 @@ const CreateMaterial = () => {
                     className="text-[14px] w-[256px] bg-[#edf1fa] text-[#8792A4] rounded-lg border-ring-0 border-[1px] px-4 py-[8px] cursor-text outline-blue-500"
                     type="text"
                     placeholder="Brand"
-                    {...register("brand", {required: true})}
-                   
+                    {...register("brand", { required: true })}
                   />
-                   {errors.Brand && <p className="text-sm text-red-500 italic">Brand is required.</p>}
+                  {errors.Brand && (
+                    <p className="text-sm text-red-500 italic">
+                      Brand is required.
+                    </p>
+                  )}
                 </div>
               </div>
 
@@ -83,12 +103,15 @@ const CreateMaterial = () => {
                     className="text-[14px] w-[256px] bg-[#edf1fa] text-[#8792A4] rounded-lg border-ring-0 border-[1px] px-4 py-[8px] cursor-text outline-blue-500"
                     type="text"
                     placeholder="Unit"
-                    {...register("unit", {required: true})}
+                    {...register("unit", { required: true })}
                   />
-                  {errors.unit && <p className="text-sm text-red-500 italic">Unit is required.</p>}
+                  {errors.unit && (
+                    <p className="text-sm text-red-500 italic">
+                      Unit is required.
+                    </p>
+                  )}
                 </div>
               </div>
-
 
               <div className=" flex flex-col gap-1 items-start justify-start">
                 <p className=" text-sm">Length</p>
@@ -97,7 +120,7 @@ const CreateMaterial = () => {
                     className="text-[14px] w-[256px] bg-[#edf1fa] text-[#8792A4] rounded-lg border-ring-0 border-[1px] px-4 py-[8px] cursor-text outline-blue-500"
                     type="text"
                     placeholder="Length"
-                    {...register("length", {required: true})}
+                    {...register("length", { required: true })}
                   />
                   {errors.length && <p>Lenght is required.</p>}
                 </div>
@@ -110,7 +133,7 @@ const CreateMaterial = () => {
                     className="text-[14px] w-[256px] bg-[#edf1fa] text-[#8792A4] rounded-lg border-ring-0 border-[1px] px-4 py-[8px] cursor-text outline-blue-500"
                     type="text"
                     placeholder="Width"
-                    {...register("width", {required: true})}
+                    {...register("width", { required: true })}
                   />
                   {errors.width && <p>Width is required.</p>}
                 </div>
@@ -123,7 +146,7 @@ const CreateMaterial = () => {
                     className="text-[14px] w-[256px] bg-[#edf1fa] text-[#8792A4] rounded-lg border-ring-0 border-[1px] px-4 py-[8px] cursor-text outline-blue-500"
                     type="text"
                     placeholder="Height"
-                    {...register("height", {required: true})}
+                    {...register("height", { required: true })}
                   />
                   {errors.height && <p>Height is required.</p>}
                 </div>
@@ -136,18 +159,12 @@ const CreateMaterial = () => {
                     className="text-[14px] w-[256px] bg-[#edf1fa] text-[#8792A4] rounded-lg border-ring-0 border-[1px] px-4 py-[8px] cursor-text outline-blue-500"
                     type="text"
                     placeholder="Weight"
-                    {...register("weight", {required: true})}
+                    {...register("weight", { required: true })}
                   />
                   {errors.weight && <p>Weight is required.</p>}
                 </div>
               </div>
-
-              
-              
-            
             </div>
-
-         
 
             <button
               htmlFor="create_new_material_modal"
@@ -158,17 +175,21 @@ const CreateMaterial = () => {
             >
               ✕
             </button>
-            
-            {loading?<div className="flex items-center mt-auto justify-end">
-              <Spin />
-            </div>:<div className="flex items-center mt-auto justify-end">
-              <button
-                type="submit"
-                className="px-4 py-2 bg-accent-0 text-white rounded-md"
-              >
-               Add Material
-              </button>
-            </div>}
+
+            {loading ? (
+              <div className="flex items-center mt-auto justify-end">
+                <Spin />
+              </div>
+            ) : (
+              <div className="flex items-center mt-auto justify-end">
+                <button
+                  type="submit"
+                  className="px-4 py-2 bg-accent-0 text-white rounded-md"
+                >
+                  Add Material
+                </button>
+              </div>
+            )}
           </form>
         </div>
       </div>
