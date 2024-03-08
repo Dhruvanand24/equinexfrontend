67e03d6699bfec8c2f974e2c254aaf6a9e68e5b6
Md Kaif Manzar <kaifmanzar321@gmail.com>
2024-03-08 16:42:49 +0530

    sdfa

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import useGetUser from '../hooks/useGetUser';
 
 import { Button, Dropdown } from 'antd';
+import { useSelector } from 'react-redux';
 
 const Navbar = () => {
 
@@ -23,12 +24,14 @@ const Navbar = () => {
     
   ];
   const { user, loading } = useGetUser();
+  const kaifuser=useSelector(state=>state.user.currentUser);
+  
   
   return (
     <div className='w-full flex h-[70px] px-4 items-center justify-between bg-white border-b'>
       <h1 className='font-[400] text-[24px] text-[#1A202C]'>Bird machines Pvt.Ltd (Bhagola)</h1>
       <Dropdown menu={{ items }} placement="bottomLeft">
-      <Button>{user?.fullName}</Button>
+      <Button>{kaifuser?.user?.fullName}</Button>
       </Dropdown>
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/router'
-
+import { Provider } from 'react-redux';
+import store from './store/userStore';
 import axios from 'axios'
 axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
-  <RouterProvider router={router} />
-  
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 )
