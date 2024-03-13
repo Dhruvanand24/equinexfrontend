870a9f0e70d92bc913cd0319b2eff97524bf0293
Dhruv_Anand <ananddhruv475@gmail.com>
2024-03-13 15:51:47 +0530

    sa

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../components/Sidebar';
 import Navbar from '../components/Navbar';
+import TempSidebar from '../components/TempSidebar';
+
 
 
 
@@ -9,7 +11,8 @@ const Main = () => {
   
   return (
       <div className='bg-background-0 font-roboto flex'>
-        <Sidebar />
+        {/* <Sidebar /> */}
+        <TempSidebar />
         <div className='flex flex-col w-full bg-background-0 pl-5'>
           <Navbar />
           <Outlet />
