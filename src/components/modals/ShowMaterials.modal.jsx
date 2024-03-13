0e50be6c986c0dc09cd20eb75ba5445197540e37
Md Kaif Manzar <kaifmanzar321@gmail.com>
2024-03-13 12:36:19 +0530

    got name by id

--- src/components/modals/ShowMaterials.modal.jsx
+++ src/components/modals/ShowMaterials.modal.jsx
@@ -1,11 +1,47 @@
 import { Table } from "antd";
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 const ShowMaterials = (props) => {
   const [allData, setAllData] = useState([]);
-  
+
+  const fetchdata = async () => {
+
+    console.log("hiii")
+    try {
+      
+      props.data.forEach(async (e, index) => {
+        const material=await axios.post("http://localhost:8000/api/v1/material/getmaterialbyid",{
+          material_id:e.material_id
+        }, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+
+        console.log(material.data);
+
+        const data = {
+          "S.no": index + 1,
+          name: material.data.data.Name,
+          material_id: e?.material_id,
+          quantity:e.quantity
+        };
+
+        console.log("data", data);
+
+        // Corrected: Use array spread to append the new data object to the dataSource array
+        setAllData((prevDataSource) => [...prevDataSource, data]);
+      });
+
+      console.log(allData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
-    setAllData(props.data);
+    fetchdata();
   }, [props.data]);
 
   const columns = [
@@ -43,8 +79,12 @@ const ShowMaterials = (props) => {
             <hr />
             <Table columns={columns} dataSource={allData} />
             <div className="flex justify-evenly mt-8">
-              <button className="bg-approved-0 text-approvedtext-0 bg-opacity-15 p-2 px-4 rounded-md">Approve</button>
-              <button className="bg-pending-0 text-pending-0 bg-opacity-15 p-2 px-4 rounded-md">Delete</button>
+              <button className="bg-approved-0 text-approvedtext-0 bg-opacity-15 p-2 px-4 rounded-md">
+                Approve
+              </button>
+              <button className="bg-pending-0 text-pending-0 bg-opacity-15 p-2 px-4 rounded-md">
+                Delete
+              </button>
             </div>
             <button
               htmlFor="show_material_modal"
