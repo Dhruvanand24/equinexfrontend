7fa2f79cfad9cf72ba4d51545bb4289fcccb6dc0
Md Kaif Manzar <kaifmanzar321@gmail.com>
2024-03-14 14:56:29 +0530

    nb

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -46,7 +46,7 @@ const router = createBrowserRouter([
         element: <AllPurchaseRequest />,
       },
       {
-        path:"/approval/materialrequest",
+        path:"approval/materialrequest",
         element:<MaterialRequestApproval/>
       },
       {
